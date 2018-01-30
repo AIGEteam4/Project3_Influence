9217185eda9c8875f1d895356cbca917b077bb9f
Ryan Geyer <gyanreyer@gmail.com>
2018-01-30 00:34:49 -0500

    Character and camera implemented

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    //Enum for camera modes - can follow character or be moved independently
+    enum CameraMode
+    {
+        followCharacter,
+        freeCam
+    }
+
+    private CameraMode mode;//Current mode
+
+    public GameObject character;//Reference to character
+    private Camera cam;//Reference to camera component for easier use
+
+    //Vars to keep track when user clicks and drags on screen in free cam
+    private Vector3 clickOrigin;//Origin pos of mouse on click
+    private Vector3 dragCamPosOrigin;//Origin pos of camera on click
+    private bool clickHeld;//Whether click is being held
+
+    //Move speed for free cam
+    public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+        mode = CameraMode.followCharacter;
+        clickHeld = false;
+
+        cam = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Follow character mode
+        if(mode == CameraMode.followCharacter)
+        {
+            //Follow character's position so always stay in center of screen
+            transform.position = new Vector3(character.transform.position.x, character.transform.position.y + 50, character.transform.position.z - 50);
+
+            //Switch modes if user presses 2
+            if(Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                mode = CameraMode.freeCam;
+            }
+        }
+        //Free cam mode
+        else
+        {
+            //Move camera around with arrow keys so that character can still be independently controlled
+            if(Input.GetKey(KeyCode.UpArrow))
+            {
+                transform.position += Vector3.forward * moveSpeed * Time.deltaTime;
+            }
+            else if(Input.GetKey(KeyCode.DownArrow))
+            {
+                transform.position += Vector3.back * moveSpeed * Time.deltaTime;
+            }
+
+            if(Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+            }
+            else if(Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+            }
+
+            //Pan camera on click and drag
+            if(Input.GetMouseButton(0))
+            {
+                //If button just pressed, store initial pos of click and camera
+                if(!clickHeld)
+                {
+                    clickHeld = true;
+                    clickOrigin = cam.ScreenToViewportPoint(Input.mousePosition);//Convert mouse pos to viewport pos for easier use
+                    dragCamPosOrigin = transform.position;
+                }
+                //Move camera based on mouse movement
+                else
+                {
+                    //Difference calculated based on double the othographic size of cam so mouse movement maps to camera movement better
+                    Vector3 diff = 2 * cam.orthographicSize * (cam.ScreenToViewportPoint(Input.mousePosition) - clickOrigin);
+                    transform.position = dragCamPosOrigin - new Vector3(diff.x, 0, diff.y);
+                }
+            }
+            //Reset when click released
+            else if(Input.GetMouseButtonUp(0))
+            {
+                clickHeld = false;
+            }
+
+            //Switch mode if user presses 1
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                mode = CameraMode.followCharacter;
+            }
+        }
+
+        //Use scroll wheel to zoom camera out/increase orthographic size
+        if (Input.mouseScrollDelta.y != 0)
+        {
+            cam.orthographicSize = Mathf.Clamp(cam.orthographicSize - Input.mouseScrollDelta.y, 5, 75);
+        }
+    }
+}
---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+
+    public float moveSpeed;//Move speed for character
+
+    // Use this for initialization
+    void Start () {
+        //Set up initial position for terrain
+        Vector3 pos = transform.position;
+        pos.y = Terrain.activeTerrain.SampleHeight(pos);
+        transform.position = pos;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 moveVec = Vector3.zero;//Vector representing any movements character needs to make
+
+        //If user presses WASD, set x and z values of move vec for appropriate direction
+        if (Input.GetKey(KeyCode.W))
+        {
+            moveVec.z = 1;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            moveVec.z = -1;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveVec.x = 1;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            moveVec.x = -1;
+        }
+
+        //If we're moving this frame, time to do some math to make steep terrain impassible
+        if(moveVec.x != 0 || moveVec.z != 0)
+        {
+            moveVec = moveVec.normalized;
+
+            //Send raycast down to terrain to get its normal where we're standing
+            RaycastHit hit;
+            Vector3 terrainNormal = Vector3.zero;
+
+            if (Physics.Raycast(transform.position + Vector3.up, Vector3.down, out hit))
+            {
+                terrainNormal = 1.1f * hit.normal;
+                terrainNormal.y = 0;
+            }
+
+            //If moving against the slope, apply the terrain normals to movement to slow it
+            if(Mathf.Sign(moveVec.x) != Mathf.Sign(terrainNormal.x))
+            {
+                moveVec.x += terrainNormal.x;
+            }
+            if(Mathf.Sign(moveVec.z) != Mathf.Sign(terrainNormal.z))
+            {
+                moveVec.z += terrainNormal.z;
+            }
+
+            //Apply move speed to move vec
+            moveVec *= moveSpeed * Time.deltaTime;
+
+            Vector3 newPos = transform.position + moveVec;//Get final new pos
+            newPos.y = Terrain.activeTerrain.SampleHeight(newPos);//Get y pos for terrain height
+
+            transform.position = newPos;//Update character position
+        }
+    }
+}

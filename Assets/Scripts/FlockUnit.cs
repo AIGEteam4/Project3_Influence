3b1b7dd9f2d3d6fbd87afe84e72385cd0410d0eb
Ryan Geyer <gyanreyer@gmail.com>
2018-02-28 14:23:12 -0500

    Set up basic skeleton to work off of

---
+++ Assets/Scripts/FlockUnit.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum FlockUnitMode
+{
+    flock//Insert more modes here?
+}
+
+public class FlockUnit : MonoBehaviour {
+
+    public FlockUnitMode mode;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        switch(mode)
+        {
+            case FlockUnitMode.flock:
+                UpdateFlock();
+                break;
+        }
+	}
+
+    void UpdateFlock()
+    {
+        //Flocking stuff goes here
+    }
+}

7d74b39166dc73cfa0d5b2bcb2d0758c72436be7
Logan <ll8875@rit.edu>
2018-03-03 22:24:15 -0500

    Basic Node setup

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Node : System.Object
+{
+    public Vector3 position;
+    public float radius;
+
+    public Node()
+    {
+        position = Vector3.zero;
+        radius = 10.0f;
+    }
+    public Node(Vector3 pos, float rad)
+    {
+        position = pos;
+        radius = rad;
+    }
+}

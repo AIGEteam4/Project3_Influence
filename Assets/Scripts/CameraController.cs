4fa0003131e82475ecc54a192050b6c583667083
Ryan Geyer <gyanreyer@gmail.com>
2018-04-13 17:42:23 -0400

    Added initial grid stuff - just add GridManager prefab to scene

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour {
+public class CameraController : MonoBehaviour
+{
     //Enum for camera modes - can follow character or be moved independently
     enum CameraMode
     {
@@ -25,94 +26,77 @@ public class CameraController : MonoBehaviour {
 
     private float zoom;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         mode = CameraMode.followCharacter;
         clickHeld = false;
 
         cam = GetComponent<Camera>();
 
-        zoom = 10f;
-        transform.position = new Vector3(0,zoom,zoom);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        //Follow character mode
-        if(mode == CameraMode.followCharacter)
-        {
-            //Follow character's position so always stay in center of screen
-            transform.position = new Vector3(character.transform.position.x, character.transform.position.y + zoom, character.transform.position.z - zoom);
+        zoom = 100f;
+        cam.orthographicSize = zoom;
+    }
 
-            //Switch modes if user presses 2
-            if(Input.GetKeyDown(KeyCode.Alpha2))
+    // Update is called once per frame
+    void Update()
+    {
+        //Move camera around with arrow keys so that character can still be independently controlled
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += Vector3.forward * moveSpeed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += Vector3.back * moveSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+        }
+
+        //Pan camera on click and drag
+        if (Input.GetMouseButton(0))
+        {
+            //If button just pressed, store initial pos of click and camera
+            if (!clickHeld)
             {
-                mode = CameraMode.freeCam;
+                clickHeld = true;
+                clickOrigin = cam.ScreenToViewportPoint(Input.mousePosition);//Convert mouse pos to viewport pos for easier use
+                dragCamPosOrigin = transform.position;
+            }
+            //Move camera based on mouse movement
+            else
+            {
+                //Difference calculated based on double the othographic size of cam so mouse movement maps to camera movement better
+                Vector3 diff = 2 * zoom * (cam.ScreenToViewportPoint(Input.mousePosition) - clickOrigin);
+                transform.position = dragCamPosOrigin - new Vector3(diff.x, 0, diff.y);
             }
         }
-        //Free cam mode
-        else
+        //Reset when click released
+        else if (Input.GetMouseButtonUp(0))
         {
-            //Move camera around with arrow keys so that character can still be independently controlled
-            if(Input.GetKey(KeyCode.UpArrow))
-            {
-                transform.position += Vector3.forward * moveSpeed * Time.deltaTime;
-            }
-            else if(Input.GetKey(KeyCode.DownArrow))
-            {
-                transform.position += Vector3.back * moveSpeed * Time.deltaTime;
-            }
+            clickHeld = false;
+        }
 
-            if(Input.GetKey(KeyCode.RightArrow))
-            {
-                transform.position += Vector3.right * moveSpeed * Time.deltaTime;
-            }
-            else if(Input.GetKey(KeyCode.LeftArrow))
-            {
-                transform.position += Vector3.left * moveSpeed * Time.deltaTime;
-            }
-
-            //Pan camera on click and drag
-            if(Input.GetMouseButton(0))
-            {
-                //If button just pressed, store initial pos of click and camera
-                if(!clickHeld)
-                {
-                    clickHeld = true;
-                    clickOrigin = cam.ScreenToViewportPoint(Input.mousePosition);//Convert mouse pos to viewport pos for easier use
-                    dragCamPosOrigin = transform.position;
-                }
-                //Move camera based on mouse movement
-                else
-                {
-                    //Difference calculated based on double the othographic size of cam so mouse movement maps to camera movement better
-                    Vector3 diff = 2 * zoom * (cam.ScreenToViewportPoint(Input.mousePosition) - clickOrigin);
-                    transform.position = dragCamPosOrigin - new Vector3(diff.x, 0, diff.y);
-                }
-            }
-            //Reset when click released
-            else if(Input.GetMouseButtonUp(0))
-            {
-                clickHeld = false;
-            }
-
-            //Switch mode if user presses 1
-            if (Input.GetKeyDown(KeyCode.Alpha1))
-            {
-                mode = CameraMode.followCharacter;
-            }
+        //Switch mode if user presses 1
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            mode = CameraMode.followCharacter;
         }
 
         //Use scroll wheel to zoom camera out/increase orthographic size
         if (Input.mouseScrollDelta.y != 0)
         {
-            float oldZoom = zoom;
-            zoom = Mathf.Clamp(zoom-Input.mouseScrollDelta.y, 3f, 75f);
+            zoom = Mathf.Clamp(zoom - Input.mouseScrollDelta.y, 10f, 200f);
+            cam.orthographicSize = zoom;
 
-            if (mode == CameraMode.followCharacter)
-                transform.position = new Vector3(transform.position.x, character.transform.position.y + zoom, character.transform.position.z - zoom);
-            else
-                transform.position = new Vector3(transform.position.x, transform.position.y + (zoom-oldZoom), transform.position.z - (zoom-oldZoom));
+            //transform.position = new Vector3(transform.position.x,zoom,transform.position.z);
         }
     }
 }
